a7d4ac246920719174cdad869172484a80999866
Ansters26 <atharva2004pandey@gmail.com>
2025-06-10 16:19:18 +0530

    captain authentication done

--- backend/app.js
+++ backend/app.js
@@ -5,12 +5,14 @@ const app = express();
 const DBconnection = require('./db/db');
 DBconnection();
 const userRoutes = require('./routes/user.routes')
+const captainRoutes = require('./routes/captain.routes');
 const cookieparser = require('cookie-parser');
 
 app.use(cookieparser());
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.use('/user',userRoutes);
+app.use('/captains',captainRoutes);
 
 
 module.exports = app;
