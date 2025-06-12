1d60d32b8153b583926e2c89c1dc9916b1f0ba67
Ansters26 <atharva2004pandey@gmail.com>
2025-06-12 21:15:14 +0530

    ride creation backend done and gomaps api integration done

--- backend/app.js
+++ backend/app.js
@@ -7,6 +7,8 @@ const DBconnection = require('./db/db');
 DBconnection();
 const userRoutes = require('./routes/user.routes')
 const captainRoutes = require('./routes/captain.routes');
+const mapsRoutes = require('./routes/maps.routes');
+const rideRoutes = require('./routes/ride.routes');
 const cookieparser = require('cookie-parser');
 app.use(cors());
 app.use(cookieparser());
@@ -14,6 +16,8 @@ app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.use('/users',userRoutes);
 app.use('/captains',captainRoutes);
+app.use('/maps', mapsRoutes);
+app.use('/rides', rideRoutes);
 
 
 module.exports = app;
